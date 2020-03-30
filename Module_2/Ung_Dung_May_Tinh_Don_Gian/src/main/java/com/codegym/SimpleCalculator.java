274b8b3e65e55f4a5315944b7ee5075e244edcdb
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-03-30 15:03:49 +0700

    Add files via upload

---
+++ Module_2/Ung_Dung_May_Tinh_Don_Gian/src/main/java/com/codegym/SimpleCalculator.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        //throw new UnsupportedOperationException("This operator has not supported");
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        //throw new UnsupportedOperationException("This operator has not been supported");
+        return first - second;
+    }
+}
+
+
