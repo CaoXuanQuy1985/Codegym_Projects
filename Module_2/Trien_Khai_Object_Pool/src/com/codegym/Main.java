af2880ad83c5efab32bba2a22c4cc9d58f2f0366
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-04-15 23:02:57 +0700

    Add files via upload

---
+++ Module_2/Trien_Khai_Object_Pool/src/com/codegym/Main.java
@@ -0,0 +1,22 @@
+package com.codegym;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Do something...
+    
+        // Create the ConnectionPool:
+        JDBCConnectionPool pool = new JDBCConnectionPool(
+          "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+          "sa", "secret");
+    
+        // Get a connection:
+        Connection con = pool.checkOut();
+    
+        // Use the connection
+        // Return the connection:
+        pool.checkIn(con);
+     
+      }
+}
