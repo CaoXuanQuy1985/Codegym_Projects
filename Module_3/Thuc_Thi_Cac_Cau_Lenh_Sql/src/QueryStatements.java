2ccbaf7a92e9c87674b3738e8fcb7004b1548dfe
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-04-28 22:33:46 +0700

    Add files via upload

---
+++ Module_3/Thuc_Thi_Cac_Cau_Lenh_Sql/src/QueryStatements.java
@@ -0,0 +1,10 @@
+public class QueryStatements {
+    public static void main(String[] args) {
+        //select * from customers;
+        //select customerName, phone, city, country from customers;
+        //select customerName from customers where customerName='Blauer See Auto, Co.';
+        //select * from customers where customerName='Blauer See Auto, Co.';
+        //select * from customers where customerName like '%A%'
+        //select * from customers where city in ('Nantes', 'Las Vegas', 'Warszawa', 'NYC');
+    }
+}
