4015bc64ba124acb4af0bf8eac5f560a02072792
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-05-07 08:37:51 +0700

    Add files via upload

---
+++ Module_3/Store_Procedure/src/ProcedureStore.java
@@ -0,0 +1,22 @@
+public class ProcedureStore {
+    public static void main(String[] args) {
+    /*    use classicmodels;
+
+        delimiter //
+        create procedure findAllCustomer()
+        begin
+        select * from customers;
+        end;
+        delimiter //
+
+                delimiter //
+        drop procedure if exists findAllCustomer //
+        create procedure findAllCustomer()
+        begin
+        select * from customers where customerName = 'Atelier graphique';
+        end;
+        delimiter //
+
+        call findAllCustomer();*/
+    }
+}
