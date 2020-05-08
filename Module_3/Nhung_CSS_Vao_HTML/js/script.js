c61ac9ed91377a3b0e0a8821709830f76a6d9fca
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-05-08 10:25:55 +0700

    Add files via upload

---
+++ Module_3/Nhung_CSS_Vao_HTML/js/script.js
@@ -0,0 +1,4 @@
+let posLink = document.getElementById("activities");
+posLink.addEventListener("click", function() {
+    $("#body").load("http://localhost:63342/Nhung_CSS_Vao_HTML/activities.html");
+}, false);
