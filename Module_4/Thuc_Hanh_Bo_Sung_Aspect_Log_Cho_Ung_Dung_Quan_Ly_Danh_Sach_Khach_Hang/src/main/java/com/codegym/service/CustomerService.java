70c08d66a1d0f9dd42373078bffad2749e965bd9
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-06-07 16:46:52 +0700

    Add files via upload

---
+++ Module_4/Thuc_Hanh_Bo_Sung_Aspect_Log_Cho_Ung_Dung_Quan_Ly_Danh_Sach_Khach_Hang/src/main/java/com/codegym/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerService {
+    Iterable<Customer>  findAllByProvince(Province province);
+
+    Page<Customer> findAllByFirstNameContaining(String firstName, Pageable pageable);
+
+    /*Page<Customer> findAll(Pageable pageable) throws Exception;*/
+
+    Page<Customer> findAll(Pageable pageable);
+
+    Customer findById(Long id) throws Exception;
+
+    void save(Customer customer);
+
+    void remove(Long id);
+}
