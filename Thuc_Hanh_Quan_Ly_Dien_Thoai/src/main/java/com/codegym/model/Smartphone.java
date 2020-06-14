bd7fc3e160386d6519c778459fe604b20acde81b
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-06-14 16:25:02 +0700

    Add files via upload

---
+++ Thuc_Hanh_Quan_Ly_Dien_Thoai/src/main/java/com/codegym/model/Smartphone.java
@@ -0,0 +1,52 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="smartphones")
+public class Smartphone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String producer;
+    private String model;
+    private String price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return producer+": "+model+" with price "+price;
+    }
+}
