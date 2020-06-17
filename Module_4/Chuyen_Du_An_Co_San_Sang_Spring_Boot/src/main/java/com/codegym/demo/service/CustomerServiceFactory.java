788db91ec74161b7d2b59a42c29fec6271fcb77c
CaoXuanQuy1985 <61116705+CaoXuanQuy1985@users.noreply.github.com>
2020-06-17 14:13:30 +0700

    Add files via upload

---
+++ Module_4/Chuyen_Du_An_Co_San_Sang_Spring_Boot/src/main/java/com/codegym/demo/service/CustomerServiceFactory.java
@@ -0,0 +1,14 @@
+package com.codegym.demo.service;
+
+import com.codegym.demo.service.impl.SimpleCustomerServiceImpl;
+
+public class CustomerServiceFactory {
+    private static CustomerService singleton;
+
+    public static synchronized CustomerService getInstance() {
+        if (singleton == null) {
+            singleton = new SimpleCustomerServiceImpl();
+        }
+        return singleton;
+    }
+}
